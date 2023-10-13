d7f188a47c54d7125c61bd7c26bd7027ca11f457
nekmichalakis <nekmichalakis@gmail.com>
2023-10-13 14:47:18 +0300

    refactored

---
+++ app.js
@@ -0,0 +1,24 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blog')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blog', blogRouter)
+
+module.exports = app
